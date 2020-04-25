d46b3131f861cade178862ab97ecf619cbb38529
slalom98 <salomeb69@gmail.com>
2020-04-25 16:21:53 +0200

    Update billet.php

--- front/billet.php
+++ front/billet.php
@@ -15,13 +15,17 @@ class PDF extends FPDF {
     $this->Ln(20);
 
     // Titre gras (B) police Helbetica de 11
-    $this->SetFont('Helvetica','B',11);
+    $this->SetFont('Helvetica','B',20);
     // fond de couleur gris (valeurs en RGB)
     $this->setFillColor(230,230,230);
      // position du coin supérieur gauche par rapport à la marge gauche (mm)
     $this->SetX(70);
     // Texte : 60 >largeur ligne, 8 >hauteur ligne. Premier 0 >pas de bordure, 1 >retour à la ligneensuite, C >centrer texte, 1> couleur de fond ok
-    $this->Cell(60,8,'Billet tournoi',0,1,'C',1);
+    $this->Cell(60,8,'Billet tournoi 2020',0,1,'C',0);
+    $this->SetFont('Helvetica','B',18);
+    $this->Cell(180,8,'Open Sopra Steria',0,1,'C',0);
+    $this->Image('../images/filetdetennis.jpeg',0,150,210);
+    $this->Image('../images/balletennis.jpeg',195,2,-500);
     // Saut de ligne 10 mm
     $this->Ln(10);
   }
@@ -54,11 +58,43 @@ class PDF extends FPDF {
 
 
     // couleur de fond de la cellule : gris clair
-    $pdf->setFillColor(230,230,230);
+    $pdf->setFillColor(100,100,100);
+    $pdf->SetFont('Helvetica','B',13);
+    $pdf -> Cell(190,6,'INFORMATIONS GENERALES DU CLIENT',0,1,'C',0);
+    $pdf->Ln(5);
     // Cellule avec les données du sous-titre sur 2 lignes, pas de bordure mais couleur de fond grise
-    $pdf->Cell(75,6,' mail client: '.$_SESSION['mail'],0,1,'L',1);
+    $pdf->SetFont('Helvetica','',11);
+    $pdf->Cell(75,6,'Mail client: '.$_SESSION['mail'],0,0,'L',0);
+    $pdf->Cell(79,6,'Telephone client : ',0,1,'R',0);
+    $pdf->Ln(2);
+    $pdf->Cell(75,6,'Nom client :',0,0,'L',0);
+    $pdf->Cell(75,6,'Prenom client : ',0,1,'R',0);
+    $pdf->Ln(2);
+    $pdf->Cell(75,6,'Adresse client : ',0,1,'L',0);
     //$pdf->Cell(75,6,strtoupper(utf8_decode($data_voyageur['prenom'].' '.$data_voyageur['nom'])),0,1,'L',1);
-    $pdf->Ln(10); // saut de ligne 10mm
+    $pdf->Ln(5); // saut de ligne 10mm
+    $pdf->SetFont('Helvetica','B',13);
+    $pdf->Cell(190,6,'INFORMATIONS COMMANDE',0,1,'C',0);
+    $pdf->Ln(5);
+    $pdf->SetFont('Helvetica','',11);
+    $pdf->Cell(75,6,'Match : '.$_SESSION['libellematch'],0,0,'L',0);
+    $pdf->Cell(75,6,'Date du match :',0,1,'R',0);
+    $pdf->Ln(2);
+    $pdf->Cell(75,6,'Emplacement : '.$_SESSION['libelleemplacement'],0,0,'L',0);
+    $pdf->Cell(81,6,'Creneau du match : ',0,1,'R',0);
+    $pdf->Ln(2);
+    $pdf->Cell(75,6,'Type de billet : '.$_SESSION['libelletbillet'],0,1,'L',0);
+    $pdf-> Ln(10);
+    $pdf->SetFont('Helvetica','B',13);
+    $pdf->Cell(190,6,'PRIX TOTAL',0,1,'C',0);
+    $pdf->SetFont('Helvetica','',12);
+    $pdf->Cell(190,6,''.$_SESSION['prixtotal'],0,0,'C',0);
+
+
+
+
+
+
 
 
 
